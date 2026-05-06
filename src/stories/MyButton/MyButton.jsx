2f62d20abe5ade4015e59a57fdf72d714a2e0894
Jonathan Choi <jonathanchoi@Jonathans-MacBook-Pro.local>
2026-05-06 12:18:22 -0500

    fix(components): update MyButton with Link wrapper and token fixes

--- src/stories/MyButton/MyButton.jsx
+++ src/stories/MyButton/MyButton.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import styles from './MyButton.module.css';
+import { Link } from 'react-router-dom';
 
 /** Primary UI component for button */
 export const MyButton = ({
@@ -12,15 +13,26 @@ export const MyButton = ({
 }) => {
   const mode = primary ? styles.primary : styles.secondary;
 
-  return (
-    <button
-      type="button"
-      className={[styles.button, styles[size], mode, className].join(' ')}
-      {...props}
-    >
-      {label}
-    </button>
-  );
+  const ButtonElement = (
+  <button
+    type="button"
+    className={[styles.button, styles[size], mode, className].join(' ')}
+    {...props}
+  >
+    {label}
+  </button>
+);
+
+  // If href is provided, wrap in Link for navigation
+  if (href) {
+    return (
+      <Link to={href}>
+        {ButtonElement}
+      </Link>
+    );
+  }
+
+  return ButtonElement;
 }
 
 MyButton.propTypes = {
